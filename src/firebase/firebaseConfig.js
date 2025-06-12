9749a10089b0cc656f18417a1b08509c37668631
juanfe <lassojuanfe@gmail.com>
2025-06-12 09:00:21 -0500

    Añade persistencia de sesión

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
+import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
@@ -16,5 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence);
 export const messaging = getMessaging(app);
 export const storage = getStorage(app);
